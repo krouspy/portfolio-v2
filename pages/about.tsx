89445459a5ca8cfdf0b6e52cc6593a0308af47eb
krouspy <knjlau1@gmail.com>
2021-12-24 00:28:40 +0100

    modify About page: make it in one column instead of two

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,37 +1,30 @@
 import type { NextPage } from 'next';
-import { Stack, Grid, GridItem, Text } from '@chakra-ui/react';
+import { Stack, Text } from '@chakra-ui/react';
 import { colors } from '@/constants';
 import { LineSpacer } from '@/components';
 
 const About: NextPage = () => {
   return (
-    <Stack px={{ base: '3%', xl: '7%' }} spacing="8">
-      <Grid templateColumns={{ base: 'repeat(1, 1fr)', xl: 'repeat(2, 1fr)' }} gap="6">
-        <GridItem colSpan={1} px={{ base: '0', xl: '10%' }}>
-          <Text fontSize="3xl" color={colors.blue} fontWeight="bold">
-            A bit about myself
-          </Text>
-          <LineSpacer lines={1} />
-          <Text fontSize="lg">
-            I am from Tahiti, French Polynesia. I recently graduated with a Master in Financial
-            Engineering from ESILV, an engineering school in Paris.
-            <LineSpacer />I discovered cryptocurrencies in 2018 starting with Bitcoin then overtime,
-            the more I learned about it, the more I understood the benefits of this technology and
-            the importance to build around it.
-          </Text>
-        </GridItem>
-        <GridItem colSpan={1}>
-          <Text fontSize="lg" px={{ base: '0', xl: '10%' }}>
-            Fast-forward to today, I had the opportunity to work at an Insurance company and to
-            collaborate on a student project with a Banking company. My goal is to keep learning
-            everyday and build things around DeFi and NFTs while exploring other ecosystems such
-            Polkadot, Avalanche, Solana and Layer 2 technologies.
-            <LineSpacer />
-            Aside from that, I really enjoy video games like League of Legends and Valorant and
-            watching animes such as Jujutsu Kaisen, Demon Slayer and Owari no Seraph
-          </Text>
-        </GridItem>
-      </Grid>
+    <Stack spacing="8">
+      <Text fontSize={{ base: '2xl', md: '3xl' }} color={colors.blue} fontWeight="bold">
+        A bit about myself
+      </Text>
+      <Text fontSize={{ base: 'sm', md: 'md', xl: 'lg' }}>
+        I am from Tahiti, French Polynesia. I recently graduated with a Master in Financial
+        Engineering from ESILV, an engineering school in Paris.
+        <LineSpacer />I discovered cryptocurrencies in 2018 starting with Bitcoin then overtime, the
+        more I learned about it, the more I understood the benefits of this technology and the
+        importance to build around it.
+      </Text>
+      <Text fontSize={{ base: 'sm', md: 'md', xl: 'lg' }}>
+        Fast-forward to today, I had the opportunity to work at an Insurance company and to
+        collaborate on a student project with a Banking company. My goal is to keep learning
+        everyday and build things around DeFi and NFTs while exploring other ecosystems such
+        Polkadot, Avalanche, Solana and Layer 2 technologies.
+        <LineSpacer />
+        Aside from that, I really enjoy video games like League of Legends and Valorant and watching
+        animes such as Jujutsu Kaisen, Demon Slayer and Owari no Seraph
+      </Text>
     </Stack>
   );
 };
