bb7ae481e032220943eeeb612c4d5eaf2390673e
krouspy <knjlau1@gmail.com>
2021-12-23 03:04:03 +0100

    add Blog page

---
+++ pages/blog.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next';
+import { Stack, Icon, Text } from '@chakra-ui/react';
+import { GiBlackBook } from 'react-icons/gi';
+
+const Blog: NextPage = () => {
+  return (
+    <Stack direction="row" justify="center" align="center" py="20%" spacing="10">
+      <Icon as={GiBlackBook} fontSize="8xl" />
+      <Text fontSize="large">Building the page once I finished writing some blog posts...</Text>
+    </Stack>
+  );
+};
+
+export default Blog;
