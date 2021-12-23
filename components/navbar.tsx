d328b7f4094b14907f2c0895f2c948f36f6dad42
krouspy <knjlau1@gmail.com>
2021-12-23 01:40:19 +0100

    facto routes values

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,48 +4,23 @@ import { Flex, Stack, Spacer, Button } from '@chakra-ui/react';
 import ActiveLink from './active-link';
 import ButtonResume from './button-resume';
 import { colors } from '@/constants';
+import routes from '@/routes';
 
-const pages = [
-  {
-    id: 0,
-    label: 'Home',
-    href: '/',
-  },
-  {
-    id: 1,
-    label: 'About',
-    href: '/about',
-  },
-  {
-    id: 2,
-    label: 'Experiences',
-    href: '/experiences',
-  },
-  {
-    id: 3,
-    label: 'Projects',
-    href: '/projects',
-  },
-  {
-    id: 4,
-    label: 'Blog',
-    href: '/blog',
-  },
-];
+const pages = Object.values(routes).map((route, index) => ({ ...route, id: index }));
 
 export const Navbar: React.FC = () => {
   return (
     <Flex w="100%" h="100%" justifyContent="center" alignItems="center">
-      <Link href="/">
+      <Link href={routes.home.href}>
         <Button variant="ghost" color={colors.blue}>
           Kenji LAU
         </Button>
       </Link>
       <Spacer />
       <Stack direction="row" spacing="10" px="1%" py="0.5%" bg="#F4F8FF" borderRadius="10">
-        {pages.map(({ id, label, href }) => (
+        {pages.map(({ id, name, href }) => (
           <ActiveLink key={id} href={href}>
-            {label}
+            {name}
           </ActiveLink>
         ))}
       </Stack>
---
+++ routes/index.ts
@@ -0,0 +1,24 @@
+const routes = {
+  home: {
+    name: 'Home',
+    href: '/',
+  },
+  about: {
+    name: 'About',
+    href: '/about',
+  },
+  experiences: {
+    name: 'Experiences',
+    href: '/experiences',
+  },
+  projects: {
+    name: 'Projects',
+    href: '/projects',
+  },
+  blog: {
+    name: 'Blog',
+    href: '/blog',
+  },
+};
+
+export default routes;
