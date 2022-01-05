9753a595ffec0375093bbcd4b55f693e3265ae38
krouspy <knjlau1@gmail.com>
2022-01-05 22:43:57 +0100

    move projects info in constants folder + add coingecko-rs

--- constants/index.ts
+++ constants/index.ts
@@ -1,3 +1,5 @@
+import { IProjectCard } from '@/types';
+
 export const colors = {
   white: '#fff',
   black: '#323232',
@@ -26,3 +28,72 @@ export const urls = {
       'https://www.crunchyroll.com/that-time-i-got-reincarnated-as-a-slime',
   },
 };
+
+export const projects: IProjectCard[] = [
+  {
+    title: 'coingecko-rs',
+    description: 'Building a Coingecko Rust client',
+    inProgress: true,
+    github: 'https://github.com/krouspy/coingecko-rs',
+    technologies: ['Rust', 'Reqwest'],
+  },
+  {
+    title: 'Twitter Clone',
+    description: 'Building a Twitter clone with Next.js, trpc, prisma and PlanetScale',
+    inProgress: true,
+    github: 'https://github.com/krouspy/twitter-clone',
+    technologies: ['Next.js', 'Trpc', 'Prisma'],
+  },
+  {
+    title: 'Portfolio V2',
+    description: 'Second version of my personal website',
+    inProgress: false,
+    github: 'https://github.com/krouspy/portfolio-v2',
+    technologies: ['Next.js', 'Vercel', 'Chakra UI'],
+  },
+  {
+    title: 'Schedular Multisig',
+    description: 'Upgradeable contract behind a Multisig contract on Acala',
+    inProgress: false,
+    github: 'https://github.com/krouspy/schedular-multisig',
+    technologies: ['Solidity', 'Hardhat', 'Acala'],
+  },
+  {
+    title: 'Portfolio',
+    description: 'First version of my personal website built with Next.js and hosted on Vercel',
+    inProgress: false,
+    github: 'https://github.com/krouspy/portfolio',
+    technologies: ['Next.js', 'Vercel', 'Chakra UI'],
+  },
+  {
+    title: 'Colloc-Chain',
+    description: 'Collocation platform built on a Hyperledger Besu private blockchain',
+    inProgress: false,
+    github: 'https://github.com/colloc-chain',
+    technologies: ['Solidity', 'Node.js', 'Mongo'],
+  },
+  {
+    title: 'Monnaies Numeriques',
+    description:
+      'Receive bitcoin payments through the lightning network and writing smart contracts on Ethereum',
+    inProgress: false,
+    github: 'https://github.com/krouspy/monnaies-numeriques',
+    technologies: ['Bitcoin', 'Ethereum'],
+  },
+  {
+    title: 'Ethereum - Selfish Mining',
+    description: 'Website that explains how selfish mining on Ethereum works and its impact',
+    inProgress: false,
+    github: 'https://github.com/krouspy/selfish-mining',
+    website: 'https://krouspy.selfish-mining.com/',
+    technologies: ['Ethereum', 'React', 'Netlify'],
+  },
+  {
+    title: 'CLightning',
+    description:
+      'Documentation explaining how to write a plugin in python for the Clightning client',
+    inProgress: false,
+    github: 'https://github.com/krouspy/clightning-plugin',
+    technologies: ['Lightning', 'Python'],
+  },
+];
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next';
 import { Stack, Grid, GridItem } from '@chakra-ui/react';
 import { ProjectCard } from '@/components';
-import { IProjectCard } from '@/types';
+import { projects } from '@/constants';
 
 const Projects: NextPage = () => {
   return (
@@ -10,8 +10,8 @@ const Projects: NextPage = () => {
         templateColumns={{ base: 'repeat(1, 1fr)', md: 'repeat(2, 1fr)', xl: 'repeat(3, 1fr)' }}
         gap="6"
       >
-        {projects.map(({ id, title, description, inProgress, github, website, technologies }) => (
-          <GridItem key={id} colSpan={1}>
+        {projects.map(({ title, description, inProgress, github, website, technologies }) => (
+          <GridItem key={title} colSpan={1}>
             <ProjectCard
               title={title}
               description={description}
@@ -28,73 +28,3 @@ const Projects: NextPage = () => {
 };
 
 export default Projects;
-
-const projects: IProjectCard[] = [
-  {
-    id: 0,
-    title: 'Twitter Clone',
-    description: 'Building a Twitter clone with Next.js, trpc, prisma and PlanetScale',
-    inProgress: true,
-    github: 'https://github.com/krouspy/twitter-clone',
-    technologies: ['Next.js', 'Trpc', 'Prisma'],
-  },
-  {
-    id: 1,
-    title: 'Portfolio V2',
-    description: 'Second version of my personal website',
-    inProgress: true,
-    github: 'https://github.com/krouspy/portfolio-v2',
-    technologies: ['Next.js', 'Vercel', 'Chakra UI'],
-  },
-  {
-    id: 2,
-    title: 'Schedular Multisig',
-    description: 'Upgradeable contract behind a Multisig contract on Acala',
-    inProgress: false,
-    github: 'https://github.com/krouspy/schedular-multisig',
-    technologies: ['Solidity', 'Hardhat', 'Acala'],
-  },
-  {
-    id: 3,
-    title: 'Portfolio',
-    description: 'First version of my personal website built with Next.js and hosted on Vercel',
-    inProgress: false,
-    github: 'https://github.com/krouspy/portfolio',
-    technologies: ['Next.js', 'Vercel', 'Chakra UI'],
-  },
-  {
-    id: 4,
-    title: 'Colloc-Chain',
-    description: 'Collocation platform built on a Hyperledger Besu private blockchain',
-    inProgress: false,
-    github: 'https://github.com/colloc-chain',
-    technologies: ['Solidity', 'Node.js', 'Mongo'],
-  },
-  {
-    id: 5,
-    title: 'Monnaies Numeriques',
-    description:
-      'Receive bitcoin payments through the lightning network and writing smart contracts on Ethereum',
-    inProgress: false,
-    github: 'https://github.com/krouspy/monnaies-numeriques',
-    technologies: ['Bitcoin', 'Ethereum'],
-  },
-  {
-    id: 6,
-    title: 'Ethereum - Selfish Mining',
-    description: 'Website that explains how selfish mining on Ethereum works and its impact',
-    inProgress: false,
-    github: 'https://github.com/krouspy/selfish-mining',
-    website: 'https://krouspy.selfish-mining.com/',
-    technologies: ['Ethereum', 'React', 'Netlify'],
-  },
-  {
-    id: 7,
-    title: 'CLightning',
-    description:
-      'Documentation explaining how to write a plugin in python for the Clightning client',
-    inProgress: false,
-    github: 'https://github.com/krouspy/clightning-plugin',
-    technologies: ['Lightning', 'Python'],
-  },
-];
