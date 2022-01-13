85289fd04103f6fc2f9977aa24f87bf29fdd84be
krouspy <knjlau1@gmail.com>
2022-01-13 17:04:17 +0100

    modify Notion layout max width

--- components/layouts/notion-layout.tsx
+++ components/layouts/notion-layout.tsx
@@ -6,7 +6,13 @@ const NotionLayout: React.FC = ({ children }) => {
   return (
     <div>
       <Head />
-      <Box w="100vw" maxW="99vw" h="100%" minH="90vh" overflowX="hidden">
+      <Box
+        w="100vw"
+        maxW={{ base: '95vw', md: '97vw', lg: '98vw', xl: '99vw' }}
+        h="100%"
+        minH="90vh"
+        overflowX="hidden"
+      >
         {children}
       </Box>
     </div>
