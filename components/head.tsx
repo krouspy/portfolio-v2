301a97a98a5e7e60e83227e765711957d86f1ba7
krouspy <knjlau1@gmail.com>
2022-01-14 20:56:09 +0100

    another attempt

--- components/head.tsx
+++ components/head.tsx
@@ -5,10 +5,15 @@ export const Head: React.FC<{ title?: string }> = ({ title }) => {
   return (
     <NextHead>
       <title>{title}</title>
+
       <meta name="author" content="Kenji LAU" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+      <meta property="og:title" content="Kenji LAU" />
       <meta property="og:description" content="I'm a Blockchain & Fullstack Developer" />
+      <meta property="og:type" content="website" />
       <meta property="og:image" content="https://kenjilau.xyz/preview.png" />
+
       <link rel="icon" href="/logo.svg" />
     </NextHead>
   );
