bb07eb0c2ad6ddb4cac6e6f1fa8f0e5ac005baf7
krouspy <knjlau1@gmail.com>
2022-01-14 20:37:56 +0100

    try to fix preview

--- components/head.tsx
+++ components/head.tsx
@@ -8,7 +8,7 @@ export const Head: React.FC<{ title?: string }> = ({ title }) => {
       <meta name="author" content="Kenji LAU" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta name="description" content="Kenji LAU's Portfolio" />
-      <meta property="image" content="/preview.png" />
+      <meta property="og:image" content="/preview.png" />
       <link rel="icon" href="/logo.svg" />
     </NextHead>
   );
