c09a0966266fd7923ce82409b094d25dbb6bc397
krouspy <knjlau1@gmail.com>
2022-01-14 20:35:37 +0100

    add preview to Head

--- components/head.tsx
+++ components/head.tsx
@@ -5,7 +5,10 @@ export const Head: React.FC<{ title?: string }> = ({ title }) => {
   return (
     <NextHead>
       <title>{title}</title>
+      <meta name="author" content="Kenji LAU" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta name="description" content="Kenji LAU's Portfolio" />
+      <meta property="image" content="/preview.png" />
       <link rel="icon" href="/logo.svg" />
     </NextHead>
   );
