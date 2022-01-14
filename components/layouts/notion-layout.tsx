9c951cfe4df280d29fc7039fb9c0b02274274478
krouspy <knjlau1@gmail.com>
2022-01-14 20:19:55 +0100

    fix table of content overflow

--- components/layouts/notion-layout.tsx
+++ components/layouts/notion-layout.tsx
@@ -1,20 +1,11 @@
 import React from 'react';
-import { Box } from '@chakra-ui/react';
 import { Head } from '@/components';
 
 const NotionLayout: React.FC = ({ children }) => {
   return (
     <div>
       <Head />
-      <Box
-        w="100vw"
-        maxW={{ base: '95vw', md: '97vw', lg: '98vw', xl: '99vw' }}
-        h="100%"
-        minH="90vh"
-        overflowX="hidden"
-      >
-        {children}
-      </Box>
+      {children}
     </div>
   );
 };
